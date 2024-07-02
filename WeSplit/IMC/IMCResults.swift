3d87557330060e329a5a89f33520a36fe871e45f
“pedroarias <pedroariasvilela@gmail.com>
2024-07-02 17:18:41 -0500

    feature: update code

--- WeSplit/IMC/IMCResults.swift
+++ WeSplit/IMC/IMCResults.swift
@@ -14,37 +14,68 @@ struct IMCResults: View {
     
     var body: some View {
         VStack {
-            Text("Tu resultado").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold().foregroundColor(.white)
-            InformationView(result:2.0)
-        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight: .infinity).background(.backgroundApp)
+            Text("Tu resultado").font(.title).bold().foregroundColor(.white)
+            let result = calculateIMC(weight: userWeight, height: userHeight)
+            InformationView(result:result)
+        }.frame(maxWidth: .infinity,maxHeight: .infinity).background(.backgroundApp)
     }
 }
 
 func calculateIMC(weight:Double , height:Double)->Double{
-    let result = weight/(height/100)
+    let result = weight/((height/100)*(height/100))
     
     return result
 }
 
-//func getImcResult(result:Double)
-
+func getImcResult(result:Double)-> (String , String, Color){
+    let title:String
+    let description:String
+    let color:Color
+    
+    switch result {
+    case 0.00...19.99:
+        title = "Peso bajo"
+        description="Estas por debajo del peso recomendado por el IMC"
+        color=Color.yellow
+    case 20.00...24.99:
+        title = "Peso normal"
+        description="Estas en el peso recomendado por el IMC"
+        color=Color.green
+    case 25.00...29.99:
+        title = "Sobrepeso"
+        description="Estas por encima del peso recomendado por el IMC"
+        color=Color.orange
+    case 30.00...100.99:
+        title = "Obesidad"
+        description="Estas nuy por encima del peso recomendado por el IMC"
+        color=Color.red
+    default:
+        title = "Error"
+        description = "Ha ocurrido un error"
+        color = Color.gray
+    }
+    
+    return (title, description , color)
+}
 struct InformationView:View {
     let result:Double
     var body: some View {
+        
+        let information = getImcResult(result: result)
         VStack{
             Spacer()
-            Text("Normal").foregroundColor(.green).font(.title).bold()
+            Text(information.0).foregroundColor(information.2).font(.title).bold()
             Spacer()
             Text("\(result, specifier: "%.2f")").font(.system(size: 80))
                 .foregroundColor(.white)
             Spacer()
-            Text("Es un resultado normal").foregroundColor(.white).font(.title2)
-            .padding(.horizontal, 8)
+            Text(information.1).foregroundColor(.white).font(.title2)
+                .padding(.horizontal, 8)
             Spacer()
         }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).background(.backgroundComponent).cornerRadius(16).padding(16)
     }
 }
 
 #Preview {
-    IMCResults(userWeight: 80 , userHeight:80)
+    IMCResults(userWeight:90 , userHeight:180)
 }
